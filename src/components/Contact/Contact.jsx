f5bc6c813e22661dd622ad1d7c67419310229d6c
Amira Nabil <amiranabil555@gmail.com>
2024-10-11 08:55:34 +0300

    1. create components 2. install bootsrap 3. implement routing in navbar

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <>
+      <h1 className="bg-success text-white">Contact us page</h1>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <>
+        <h1 className="bg-warning">Home Page</h1>
+      </>
+    );
+  };
+  
+  export default Home;
+  
