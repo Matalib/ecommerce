652506c5e4cffbe811a27b967234843c200dddb2
Nourhaneliwa <nourhaneliwa7@gmail.com>
2024-10-19 20:54:31 +0300

    product page

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,69 @@
 import React from "react";
 
-function Products() {
-  return <div>Products</div>;
-}
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import Loader from "../Loader/Loader";
 
-export default Products;
+export default function Products() {
+  const [product, setProduct] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  function getProducts() {
+    axios
+      .get("https://ecommerce.routemisr.com/api/v1/products")
+      .then(({ data }) => {
+        console.log(data.data);
+        setLoading(false);
+        setProduct(data.data);
+      })
+
+      .catch(() => {
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="container">
+      {!isLoading ? (
+        <div className="d-flex flex-wrap">
+          {product.map((productInfo) => {
+            return (
+              <>
+                <div className="w-25 px-4">
+                  <img
+                    src={productInfo.imageCover}
+                    className="w-100"
+                    alt={productInfo.title}
+                  />
+                  <span className="text-info d-block">
+                    {" "}
+                    {productInfo.category.name}{" "}
+                  </span>
+                  <span className=" d-block">
+                    {" "}
+                    {productInfo.title.split(" ").slice(0, 3).join(" ")}{" "}
+                  </span>
+
+                  <div className="d-flex justify-content-between my-2">
+                    <span>{productInfo.price} EGP</span>
+                    <span>
+                      {productInfo.ratingsQuantity}
+                      <i className="fas fa-star text-warning"></i>
+                    </span>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+}
