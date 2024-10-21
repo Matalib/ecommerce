2a57e8242af5b60bbea372a3616f082ee6ba3978
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-22 01:24:15 +0200

    Cahnge styling for productCard and handle toast & AddTocart Function

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,90 +1,86 @@
-import React, { useContext } from "react";
-
+import { useContext, useState, useEffect } from "react";
 import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
 import Loader from "../Loader/Loader";
-import { Link } from "react-router-dom";
-
 import { CartContext } from "../../context/CartContextProvider";
+import toast from "react-hot-toast";
 export default function Products() {
   const [product, setProduct] = useState([]);
   const [isLoading, setLoading] = useState(true);
-  const { addProductToCart } = useContext(CartContext);
-
-  async function addProductItem(id) {
-    let response = await addProductToCart(id);
-    console.log("response", response);
-  }
-
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  const [currentProductId, setCurrentProductId] = useState(0);
+  const { addProductToCart, cartItemsCount, setCartItemsCount } = useContext(CartContext);
   function getProducts() {
     axios
       .get("https://ecommerce.routemisr.com/api/v1/products")
       .then(({ data }) => {
-        console.log(data.data);
-        setLoading(false);
         setProduct(data.data);
+        setIsPageLoading(false);
+        setLoading(false);
       })
-
       .catch(() => {
         setLoading(false);
+        setIsPageLoading(false);
       });
   }
-
+  async function addItemToCart(id) {
+    setCurrentProductId(id);
+    setLoading(true);
+    let response = await addProductToCart(id);
+    if (response?.data?.status === "success") {
+      setCartItemsCount(cartItemsCount + 1);
+      toast.success(response.data.message, {
+        position: "top-right"
+      });
+      setLoading(false);
+    } else {
+      toast.error(response.data.message, {
+        position: "top-right"
+      });
+      setLoading(false);
+    }
+  }
   useEffect(() => {
     getProducts();
   }, []);
 
   return (
-    <div className="container mt-5 pt-5">
-      {!isLoading ? (
-        <div className="d-flex flex-wrap">
-          {product.map((productInfo) => {
-            return (
-              <>
-                <div className="w-25 px-4 styleProduct">
-                  <Link
-                    to={`/productDetails/${productInfo.id}`}
-                    className="card my-5 border-0 shadow-sm text-decoration-none"
+    <div className="container mt-5 py-5">
+      {isPageLoading ? <Loader /> :
+        <div className="product_wrapper pt-5">
+          {product.map((productInfo) => (
+            <>
+              <div className="product_card shadow-md" key={productInfo.id}>
+                <img
+                  src={productInfo.imageCover}
+                  alt={productInfo.title}
+                  loading="lazy"
+                />
+                <div className="text-center mt-4">
+                  <p className="text-success mb-0 fw-bolder">
+                    {productInfo.category.name}
+                  </p>
+                  <h4 className="mt-1">{productInfo.title.substring(0, 20)}</h4>
+                  <div className="d-flex justify-content-between align-items-center p-3">
+                    <span style={{ fontWeight: "bold" }}>
+                      {productInfo.price} EGP
+                    </span>
+                    <span>
+                      {productInfo.ratingsAverage}{" "}
+                      <i className="fas fa-star text-warning"></i>
+                    </span>
+                  </div>
+                  <button
+                    onClick={() => addItemToCart(productInfo.id)}
+                    className="btn bg-success text-white fs-5 mt-1"
                   >
-                    <img
-                      src={productInfo.imageCover}
-                      className="card-img-top w-100"
-                      alt={productInfo.title}
-                    />
-                    <span className="text-info d-block text-decoration-none px-2">
-                      {" "}
-                      {productInfo.category.name}{" "}
-                    </span>
-                    <span className=" d-block text-decoration-none px-2">
-                      {" "}
-                      {productInfo.title.split(" ").slice(0, 3).join(" ")}{" "}
-                    </span>
-
-                    <div className="d-flex justify-content-between my-2 card-body d-flex flex-column">
-                      <span>{productInfo.price} EGP</span>
-                      <span>
-                        {productInfo.ratingsQuantity}
-                        <i className="fas fa-star text-warning text-decoration-none"></i>
-                      </span>
-                    </div>
-                    <button
-                      onClick={() => {
-                        addProductItem(productInfo.id);
-                      }}
-                      className="btn bg-info text-white w-100 text-decoration-none "
-                    >
-                      Add To Cart
-                    </button>
-                  </Link>
+                    {isLoading && currentProductId === productInfo.id ? <i className="fa fa-spinner fa-spin fa-xl"></i> : "Add to cart"} <i className="fas fa-cart-plus ms-2"></i>
+                  </button>
                 </div>
-              </>
-            );
-          })}
+              </div>
+            </>
+          ))}
         </div>
-      ) : (
-        <Loader />
-      )}
+      }
     </div>
   );
 }
