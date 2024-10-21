659c8e6d3073d4cedbaa3b818fac7da3d809064a
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-22 00:12:33 +0200

    Add Not Found image and structure of notFound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+import notFoundImage from "../../assets/imgs/notFoundImg.svg";
 const NotFound = () => {
   return (
-    <>
-      <h1>NotFound</h1>
-    </>
+    <div className="container mt-5 py-5">
+      <div className="h-75 d-flex align-items-center justify-content-center flex-column gap-4 py-5">
+        <img src={notFoundImage} alt="404 Image" className="w-50" />
+        <Link to="/home">
+          <button className="btn btn-success mt-2">Back To Home</button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
