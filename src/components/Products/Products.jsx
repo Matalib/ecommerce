87a5679462742f49bbe35df3228b7a33acb4ae49
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-22 17:22:08 +0200

    Replace code of Card with ProductCard Component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ import Loader from "../Loader/Loader";
 import { CartContext } from "../../context/CartContextProvider";
 import toast from "react-hot-toast";
 import GetAllProducts from "./getAllProducts";
-import { Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
 export default function Products() {
   const [product, setProduct] = useState([]);
   const [isLoading, setLoading] = useState(true);
@@ -19,16 +19,6 @@ export default function Products() {
   const lastIndex = currentPage * productsPerPage;
   const firstIndex = lastIndex - productsPerPage;
   const displayedProducts = products.slice(firstIndex, lastIndex);
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
   function getProducts() {
     axios
       .get("https://ecommerce.routemisr.com/api/v1/products")
@@ -71,44 +61,8 @@ export default function Products() {
         displayedProducts.length > 0 && (
           <>
             <div className="product_wrapper pt-5">
-              {displayedProducts.map((productInfo) => (
-                <div className="product_card shadow-md" key={productInfo.id}>
-                  <Link to={`/productDetails/${productInfo.id}`}>
-                    <img
-                      src={productInfo.imageCover}
-                      alt={productInfo.title}
-                      loading="lazy"
-                    />
-                  </Link>
-                  <div className="text-center mt-4">
-                    <p className="text-success mb-0 fw-bolder">
-                      {productInfo.category.name}
-                    </p>
-                    <h4 className="mt-1">
-                      {productInfo.title.substring(0, 20)}
-                    </h4>
-                    <div className="d-flex justify-content-between align-items-center p-3">
-                      <span style={{ fontWeight: "bold" }}>
-                        {productInfo.price} EGP
-                      </span>
-                      <span>
-                        {productInfo.ratingsAverage}{" "}
-                        <i className="fas fa-star text-warning"></i>
-                      </span>
-                    </div>
-                    <button
-                      onClick={() => addItemToCart(productInfo.id)}
-                      className="btn bg-success text-white fs-5 mt-1"
-                    >
-                      {isLoading && currentProductId === productInfo.id ? (
-                        <i className="fa fa-spinner fa-spin fa-xl"></i>
-                      ) : (
-                        "Add to cart"
-                      )}
-                      <i className="fas fa-cart-plus ms-2"></i>
-                    </button>
-                  </div>
-                </div>
+                {displayedProducts.map((productInfo) => (
+                <ProductCard key={productInfo.id} productInfo={productInfo} addItemToCart={addItemToCart} isLoading={isLoading} currentProductId={currentProductId}/>
               ))}
             </div>
             <div className="d-flex justify-content-center align-items-center mt-5">
