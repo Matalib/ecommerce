7f0fe524852250b353df34b252c3fc0d65e80d7c
Amira Nabil <amiranabil555@gmail.com>
2024-10-22 19:27:46 +0300

    add chatbot to home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,16 @@ import ProductList from "./components/ProductList/ProductList";
 import Counter from "./components/Counter/Counter";
 import Newsletter from "./components/Newsletter/Newsletter";
 import Loader from "../Loader/Loader";
+import Chatbot from "../Chatbot/Chatbot";
+import "./Home.css";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
+  const [isChatbotVisible, setChatbotVisible] = useState(false);
+
+  const toggleChatbot = () => {
+    setChatbotVisible((prevState) => !prevState);
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -27,6 +34,15 @@ const Home = () => {
       <ProductList category="best-seller" />
       <Counter />
       <Newsletter />
+
+      <div
+        className="chatbot-button rounded-circle d-flex p-4 align-items-center justify-content-center shadow-sm mx-3"
+        onClick={toggleChatbot}
+      >
+        <i class="fa-solid fa-message text-white"></i>
+      </div>
+
+      {isChatbotVisible && <Chatbot />}
     </>
   );
 };
